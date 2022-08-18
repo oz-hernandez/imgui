9d1f38422d808628cb85c9dee477ae727cd26191
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2022-08-18 11:26:02 -0400

    add our pch file

--- backends/imgui_impl_opengl3.cpp
+++ backends/imgui_impl_opengl3.cpp
@@ -91,6 +91,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+#include "BlackCubePCH.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
--- backends/imgui_impl_opengl3.h
+++ backends/imgui_impl_opengl3.h
@@ -21,12 +21,13 @@
 #pragma once
 #include "imgui.h"      // IMGUI_IMPL_API
 
+
 // Backend API
 IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = NULL);
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
-
+//
 // (Optional) Called by Init/NewFrame/Shutdown
 IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
 IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
